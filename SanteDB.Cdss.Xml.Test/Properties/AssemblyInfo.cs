e80919aa6c1ab0585c089d0bd63bec90dac3a907
Justin Fyfe <justin.fyfe1@mohawkcollege.ca>
2018-12-08 09:47:21 +0700

    Rev'd versions

--- SanteDB.Cdss.Xml.Test/Properties/AssemblyInfo.cs
+++ SanteDB.Cdss.Xml.Test/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
-[assembly: AssemblyVersion("1.1.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
+// [assembly: AssemblyVersion("1.10.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
+[assembly: AssemblyVersion("1.10.0.*")][assembly: AssemblyInformationalVersion("Fredericton")]
 [assembly: AssemblyFileVersion("1.1.0.0")]
