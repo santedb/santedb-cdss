82f228c54e3047b7d725baf535ad9b0a7d8af0d1
Justin Fyfe <justin@fyfesoftware.ca>
2020-09-13 17:21:14 -0400

    Version 2.0.38 pre-release

--- SanteDB.Cdss.Xml.Test/Properties/AssemblyInfo.cs
+++ SanteDB.Cdss.Xml.Test/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.37.*")][assembly: AssemblyInformationalVersion("Langley")]
-[assembly: AssemblyVersion("2.0.37.*")][assembly: AssemblyInformationalVersion("Langley")]
-[assembly: AssemblyFileVersion("2.0.37.0")]
+// [assembly: AssemblyVersion("2.0.38.*")][assembly: AssemblyInformationalVersion("Langley")]
+[assembly: AssemblyVersion("2.0.38.*")][assembly: AssemblyInformationalVersion("Langley")]
+[assembly: AssemblyFileVersion("2.0.38.0")]
