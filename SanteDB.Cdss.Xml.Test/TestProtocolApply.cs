ba71b03e04fee8296f6d66e012b01071f2557755
Justin Fyfe <justin@fyfesoftware.ca>
2024-12-12 16:13:39 -0500

    Updated protocol apply

--- SanteDB.Cdss.Xml.Test/TestProtocolApply.cs
+++ SanteDB.Cdss.Xml.Test/TestProtocolApply.cs
@@ -440,7 +440,7 @@ namespace SanteDB.Cdss.Xml.Test
                 }
             }
 
-            Assert.AreEqual(8, acts.Relationships.Count());
+            Assert.AreEqual(9, acts.Relationships.Count());
 
         }
 
