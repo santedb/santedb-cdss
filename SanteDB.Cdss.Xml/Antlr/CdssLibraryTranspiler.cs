f88a8cfd0a9753c3d83f7689be5d2a2a222a6785
Trevor Davis <trevor@santesuite.com>
2026-07-24 09:54:40 -0400

    Update SharpCompress to 0.50. JIMS-1591

--- SanteDB.Cdss.Xml/Antlr/CdssLibraryTranspiler.cs
+++ SanteDB.Cdss.Xml/Antlr/CdssLibraryTranspiler.cs
@@ -24,16 +24,12 @@ using SanteDB.Cdss.Xml.Model.Actions;
 using SanteDB.Cdss.Xml.Model.Assets;
 using SanteDB.Cdss.Xml.Model.Expressions;
 using SanteDB.Core.Model.Serialization;
-using SharpCompress;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
-using SanteDB;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 
 namespace SanteDB.Cdss.Xml.Antlr
 {
